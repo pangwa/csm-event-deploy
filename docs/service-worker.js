ad435215deaa654e21591f8603c583ca2e1764c5
zikunfan <zikunvan@gmail.com>
2021-02-26 10:02:59 +0000

    Automated release: 2021-02-26, 10:02:59

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7daebf033d85e470fc186847ba870042.js"
+  "./precache-manifest.0b40600d92f6979c8ae276cf4eebc8d6.js"
 );
 
 self.addEventListener('message', (event) => {
